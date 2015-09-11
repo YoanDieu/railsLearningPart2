305dc2a9f98575d9132a086348f139b2b1317a23
YoanDieu <yoan.dieu@gmail.com>
2015-09-11 17:27:55 +0200

    added validations in forms

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    Book.find(params[:id]).update title: params[:title]
-    redirect_to "/books/#{params[:id]}"
+    @book = Book.find(params[:id])
+    if Book.find(params[:id]).update title: params[:title]
+      redirect_to "/books/#{params[:id]}"
+    else
+      render 'show'
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, presence: {
+    message: "Le titre doit être renseigné"
+  }
+
+  validates :category_id, presence: {
+    message: "Un catégorie doit être selectionnée"
+  }
 end
